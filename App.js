17c695a3f5c5dafebc9d3a036f4fd405331ac050
kvngdavid <davidoladele80@gmail.com>
2021-12-19 11:41:31 +0100

    snapshot

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from 'react-native';
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+    <HomeScreen />
+    </Provider>
+    
   );
 }
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Text>I am the HomeScreen</Text>
+		</View>
+	)
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
